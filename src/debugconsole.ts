d4118188bdd95cb342ade54faec92417dc375a97
KonjacDesert <56007373+konjacdesert@users.noreply.github.com>
2023-06-09 22:36:50 +0900

    first

---
+++ src/debugconsole.ts
@@ -0,0 +1,44 @@
+import { CanvasScreen } from "./sccreen";
+
+export const DbgConsole = (function () {
+  const messagesPerFrame: string[] = [];
+  const messages: string[] = [];
+
+  function add(mes: string) {
+    messagesPerFrame.push(mes);
+  }
+  function addPermanent(mes: string) {
+    messages.unshift(mes);
+    messages.splice(10);
+  }
+  function clear() {
+    messages.splice(0);
+  }
+  function update() {
+    const disp = CanvasScreen.getDsp();
+    disp.save();
+    disp.textAlign = "right"
+    disp.textBaseline = "hanging";
+    disp.font = "10px serif";
+    disp.fillStyle = "#0f0a";
+
+    messagesPerFrame.forEach((v, i) => {
+      disp.fillText(v, CanvasScreen.info.WIDTH, i * 10);
+    });
+
+    //恒常ログ
+    disp.textAlign = "left"
+    messages.forEach((v, i) => {
+      disp.fillText(v, 0, i * 10);
+    });
+    disp.restore();
+    messagesPerFrame.splice(0);
+  }
+
+  return {
+    add: process.env.NODE_ENV === "development" ? add : function () { },
+    addp: process.env.NODE_ENV === "development" ? addPermanent : function () { },
+    clearp: clear,
+    update: update,
+  };
+})();
